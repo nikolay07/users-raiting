876161a7ae891fec7157106e2951a1a50f3508c0
nikolay07 <nikolay0701@ukr.net>
2020-10-11 12:59:57 +0300

    Updates

---
+++ precache-manifest.1cecc18ba2b029ee862a6ca9d38a251e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13e18191dd53d2d6f0947795d40eddb",
+    "url": "/nikolay07/users-raiting/index.html"
+  },
+  {
+    "revision": "bc2283ec4808bef62c63",
+    "url": "/nikolay07/users-raiting/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "8727b55c16f7f500db76",
+    "url": "/nikolay07/users-raiting/static/css/main.88f20eb0.chunk.css"
+  },
+  {
+    "revision": "bc2283ec4808bef62c63",
+    "url": "/nikolay07/users-raiting/static/js/2.20170e13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nikolay07/users-raiting/static/js/2.20170e13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8727b55c16f7f500db76",
+    "url": "/nikolay07/users-raiting/static/js/main.9ae7b8b1.chunk.js"
+  },
+  {
+    "revision": "e37bbf3de45a4156a644",
+    "url": "/nikolay07/users-raiting/static/js/runtime-main.107d15fd.js"
+  },
+  {
+    "revision": "41b06d7c5b4a0f536891b9d6837f9074",
+    "url": "/nikolay07/users-raiting/static/media/driver.41b06d7c.jfif"
+  }
+]);
