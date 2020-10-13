5f2f4b8d6e1ef7570d0f353bbdd607cc22ba8dc4
Nik <47363595+nikolay07@users.noreply.github.com>
2020-10-13 11:48:18 +0300

    Update User.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,50 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
-const User = ({ id,  name, avatar, speed, time, color }) => {
+const User = ({ id, name, avatar, speed, time, color }) => {
+  const [active, setActive] = useState(false);
 
-  const handleClick = (e) => {
-    let id = e.currentTarget.id
-    const numOrder = document.getElementById(`num-order-${id}`); 
-    const avatarBorder = document.getElementById(`avatar-border-${id}`);
-    const time = document.getElementById(`time-${id}`);
-    const penalty = document.getElementById(`time-penalty-${id}`);
-    // numOrder.style.color = color.blueviolet;
-    // avatarBorder.style.borderColor = color.blueviolet;
-    // time.style.color = color.blueviolet;
-    // penalty.style.color = color.orange;
-    numOrder.classList.toggle("toggleViolet");
-    avatarBorder.classList.toggle("toggleBorder");
-    time.classList.toggle("toggleViolet");
-    penalty.classList.toggle("toggleOrange"); 
-    
+  const numClass = classNames("num", {
+    toggleViolet: active,
+  });
+  const avatarClass = classNames("avatar", {
+    toggleBorder: active,
+  });
+  const timeClass = classNames("time", {
+    toggleViolet: active,
+  });
+  const penaltyClass = classNames("penalty", {
+    toggleOrange: active,
+  });
+
+  const handleClick = () => {
+    setActive(!active);
   };
-  
+
   return (
     <div id={id} className="user" onClick={handleClick}>
       <div className="user__logo">
-        <span id={`num-order-${id}`} >
-          {id}
-        </span>
-        <img
-          src={avatar}
-          alt="avatar"
-          id={`avatar-border-${id}`}
-          
-        />
+        <span className={numClass}>{id}</span>
+        <img src={avatar} alt="avatar" className={avatarClass} />
       </div>
       <div className="user__info">
         <div className="user__info_name">
           <span>{name}</span>
         </div>
         <div className="user__info_speed">
-          <span id={`time-${id}`}>00:00.000</span>
+          <span className={timeClass}>00:00.000</span>
           <span className="speed">| {speed}км/ч</span>
         </div>
         <div className="user__info_penalty">
-          <span id={`time-penalty-${id}`} >
-            штрафное время {time}0:00.000
-          </span>
+          <span className={penaltyClass}>штрафное время {time}0:00.000</span>
         </div>
       </div>
     </div>
@@ -61,3 +54,4 @@ User.propTypes = {
   speed: PropTypes.number.isRequired,
   time: PropTypes.number.isRequired,
 };
+
